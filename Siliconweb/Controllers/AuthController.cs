542e49223ee849f822d10ed21c416b88bd934aff
Markus <Markuslokander97@gmail.com>
2024-03-08 21:17:12 +0100

    Continued the work on "SignUp" view

--- Siliconweb/Controllers/AuthController.cs
+++ Siliconweb/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Siliconweb.Models.Views;
 
 namespace Siliconweb.Controllers;
 
@@ -6,7 +7,17 @@ public class AuthController : Controller
 {
     // GET
     [Route("/signup")]
+    [HttpGet]
     public IActionResult SignUp()
+    {
+        var viewModel = new SignUpViewModel();
+        return View(viewModel);
+    }
+    
+    
+    [Route("/signup")]
+    [HttpPost]
+    public IActionResult SignUp(SignUpViewModel model)
     {
         return View();
     }
