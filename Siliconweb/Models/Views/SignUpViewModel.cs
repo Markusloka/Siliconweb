8e08b3c90be1d5057e2240adccf7186b865891d1
Markus <Markuslokander97@gmail.com>
2024-03-15 15:53:28 +0100

    Elias helps thanks

--- Siliconweb/Models/Views/SignUpViewModel.cs
+++ Siliconweb/Models/Views/SignUpViewModel.cs
@@ -2,7 +2,7 @@ namespace Siliconweb.Models.Views;
 
 public class SignUpViewModel
 {
-    public string Tilte { get; set; } = "Sign up";
+    public string Title { get; set; } = "Sign up";
     public SignUpModel Form { get; set; } = new SignUpModel();
 
     public bool TermsAndConditions { get; set; } = false;
