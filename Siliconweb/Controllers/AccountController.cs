950d9c3cb96d087752e505e2cad9f43ba2d73c01
Markusloka <Markuslokander97@gmail.com>
2024-03-27 19:57:34 +0100

    account added page and logic

--- Siliconweb/Controllers/AccountController.cs
+++ Siliconweb/Controllers/AccountController.cs
@@ -1,14 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Siliconweb.ViewModels;
 
-namespace Siliconweb.Controllers
+namespace Siliconweb.Controllers;
+
+public class AccountController : Controller
 {
-    public class AccountController : Controller
-    {
-        //private readonly AccountService _accountService;
+    //private readonly AccountService _accountService;
 
-       // public AccountController(AccountService accountService)
-       // {
-        //    _accountService = accountService;
-       // }
+    // public AccountController(AccountService accountService)
+    // {
+    //    _accountService = accountService;
+    // }
+
+[Route("/account")]
+public IActionResult Details ()
+{
+   var viewModel = new AccountDetailsViewModel();
+    return View(viewModel);
+}
+
+
+    [HttpPost]
+    public IActionResult BasicInfo(AccountDetailsViewModel viewModel)
+    {
+
+        return RedirectToAction(nameof(Details));
     }
 }
---
+++ Siliconweb/Models/AccountDetailsAdressInfoModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Siliconweb.Models
+{
+    public class AccountDetailsAdressInfoModel
+    {
+        [Display(Name = "Address line", Prompt = "Home address", Order = 0)]
+        [Required(ErrorMessage = "Invalid address")]
+
+        public string Addressline_1 { get; set; } = null!;
+
+        [Display(Name = "Secound Address line", Prompt = "Secoundary Address", Order = 1)]
+
+        public string? Addressline_2 { get; set; }
+
+
+        [Display(Name = "Postal Code", Prompt = "Enter your Postal Code", Order = 2)]
+        [Required(ErrorMessage = "Invalid postal code")]
+        [DataType(DataType.PostalCode)]
+        public string PostalCode { get; set; } = null!;
+
+        [Display(Name = "City", Prompt = "Enter your City", Order = 3)]
+        [Required(ErrorMessage = "City is required")]
+    
+
+        public string City { get; set; } = null!;
+    }
+}
