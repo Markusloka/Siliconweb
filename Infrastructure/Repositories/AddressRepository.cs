de8cedf6fa16024bcca13d49b1b89dfe3237170c
Markusloka <Markuslokander97@gmail.com>
2024-04-03 15:24:44 +0200

    Repos finished

---
+++ Infrastructure/Repositories/AddressRepository.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Infrastructure.Context;
+using Infrastructure.Entities;
+
+namespace Infrastructure.Repositories;
+
+public class AddressRepository(DataContext context) : Repo<AddressEntity>(context)
+{
+    private readonly DataContext _context = context;
+}
---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿
+
+using Infrastructure.Context;
+using Infrastructure.Entities;
+using Infrastructure.Factories;
+using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository(DataContext context) : Repo<UserEntity>(context)
+{
+    private readonly DataContext _context = context;
+
+    public override async Task<ResponseResult> GetAllAsync()
+    {
+        try
+        {
+            IEnumerable<UserEntity> result = await _context.Users
+                .Include (i => i.Address)
+                .ToListAsync();
+            return ResponseFactory.Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+
+        }
+    }
+
+    public override async Task<ResponseResult> GetOneAsync(Expression<Func<UserEntity, bool>> predicate)
+    {
+        try
+        {
+            var result = await _context.Set<UserEntity>()
+                .Include (i => i.Address)
+                .FirstOrDefaultAsync(predicate);
+            if (result == null)
+                return ResponseFactory.NotFound();
+            return ResponseFactory.Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+
+        }
+    }
+}
